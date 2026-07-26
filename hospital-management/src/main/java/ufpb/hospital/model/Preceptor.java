b5b0a1ddab1d86ba6626a85df24042df5c9e25bf
Gabriel <gabriel.rosas7232@gmail.com>
2026-07-26 19:55:36 -0300

    feat: Implementação das entidades relacionadas a Pessoa

--- hospital-management/src/main/java/ufpb/hospital/model/Preceptor.java
+++ hospital-management/src/main/java/ufpb/hospital/model/Preceptor.java
@@ -1,5 +1,19 @@
 package ufpb.hospital.model;
 
-public class Preceptor {
-    
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "preceptor")
+@PrimaryKeyJoinColumn(name = "id_pessoa")
+public class Preceptor extends Pessoa {
+
+    @Column(name = "titulacao", nullable = false, length = 50)
+    private String titulacao;
 }
