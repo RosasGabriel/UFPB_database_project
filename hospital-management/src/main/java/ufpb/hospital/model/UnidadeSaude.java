89a1d085d6f544aa0555b3ee0b40595c629f3b2d
Gabriel <gabriel.rosas7232@gmail.com>
2026-07-26 21:20:56 -0300

    feat: Implementação das entidades Escala e Unidade

--- hospital-management/src/main/java/ufpb/hospital/model/UnidadeSaude.java
+++ hospital-management/src/main/java/ufpb/hospital/model/UnidadeSaude.java
@@ -1,5 +1,31 @@
 package ufpb.hospital.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "unidade_saude")
 public class UnidadeSaude {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_unidade")
+    private Long idUnidade;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "tipo_unidade", nullable = false, length = 50)
+    private String tipoUnidade;
+
+    @Column(name = "endereco", nullable = false)
+    private String endereco;
 }
